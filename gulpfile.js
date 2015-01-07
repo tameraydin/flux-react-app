19ceb845b4ed87ec959b1b1c30c70fdd9a396fee
Tamer Aydin <tamer.aydin@gmail.com>
2015-01-08 00:50:02 +0100

    added environment support via gulp-preprocess

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var runSequence = require('gulp-run-sequence');
 var clean = require('gulp-clean');
-var header = require('gulp-header');
+var preprocess = require('gulp-preprocess');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
@@ -11,13 +11,28 @@ var PATHS = {
   DIST: 'dist/'
 };
 
+var CONTEXT = {
+  DEV: {
+    ENV: 'DEV',
+    DEBUG: true
+  },
+  PROD: {
+    ENV: 'PROD'
+  }
+};
+
+var DEVELOPMENT = true;
+
 gulp.task('clean', function() {
   return gulp.src(PATHS.DIST + '**/*.*', {read: false})
     .pipe(clean({force: true}));
 });
 
-gulp.task('copy', function() {
+gulp.task('html', function() {
   return gulp.src(PATHS.SOURCE + '*.html')
+    .pipe(preprocess({
+      context: CONTEXT[DEVELOPMENT ? 'DEV' : 'PROD']
+    }))
     .pipe(gulp.dest(PATHS.DIST));
 });
 
@@ -29,10 +44,19 @@ gulp.task('browserify', function() {
     .pipe(gulp.dest(PATHS.DIST));
 });
 
-gulp.task('build', function() {
-  runSequence(
-    'clean',
-    'copy',
-    'browserify'
-  )
+gulp.task('init', function() {
+  tasks = DEVELOPMENT ?
+    ['clean', 'html', 'browserify'] :
+    ['clean', 'html', 'browserify'];
+  runSequence.apply(this, tasks);
+});
+
+gulp.task('build', function(env) {
+  DEVELOPMENT = true;
+  gulp.run('init');
+});
+
+gulp.task('deploy', function(env) {
+  DEVELOPMENT = false;
+  gulp.run('init');
 });
--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,4 @@ var React = require('react');
 
 React.render(
   <APP />,
-  document.getElementById('main'));
+  document.getElementById('container'));
