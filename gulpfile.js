1fd3d262aba1ae6a26b1b5bd8d31a3e687e5d57b
Tamer Aydin <tamer.aydin@gmail.com>
2015-01-08 18:18:23 +0100

    - introduced gulp-usemin - fix on gulp-run-sequence

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var runSequence = require('gulp-run-sequence');
 var clean = require('gulp-clean');
-var preprocess = require('gulp-preprocess');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
@@ -9,116 +8,95 @@ var uglify = require('gulp-uglify');
 var buffer = require('vinyl-buffer');
 var rename = require('gulp-rename');
 var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
 var sass = require('gulp-sass');
+var usemin = require('gulp-usemin');
 var to5 = require('gulp-6to5'); // this also handles JSX transform
 
 var PATHS = {
   SOURCE: './src/',
-  DIST: './dist/'
+  DIST: './dist/',
+  BUILD: './build/'
 };
 
-var CONTEXT = {
-  DEV: {
-    ENV: 'DEV',
-    DEBUG: true
-  },
-  PROD: {
-    ENV: 'PROD'
-  }
-};
-
-var DEVELOPMENT = true;
+var development = true;
+var deploying = false;
 
 gulp.task('clean', function() {
-  return gulp.src(PATHS.DIST + '**/*.*', {read: false})
+  return gulp.src(PATHS.BUILD + '**/*.*', {read: false})
     .pipe(clean({force: true}));
 });
 
-gulp.task('copyHtml', function() {
-  return gulp.src(PATHS.SOURCE + '*.html')
-    .pipe(preprocess({
-      context: CONTEXT[DEVELOPMENT ? 'DEV' : 'PROD']
-    }))
-    .pipe(gulp.dest(PATHS.DIST));
-});
-
-gulp.task('minifyHtml', function() {
-  return gulp.src(PATHS.DIST + '*.html')
-    .pipe(minifyHtml())
-    .pipe(gulp.dest(PATHS.DIST));
-});
-
 gulp.task('html', function() {
-  tasks = DEVELOPMENT ?
-    ['copyHtml'] :
-    ['copyHtml', 'minifyHtml'];
-  runSequence.apply(this, tasks);
+  return gulp.src(PATHS.SOURCE + '*.html')
+    .pipe(gulp.dest(PATHS.BUILD));
 });
 
 gulp.task('img', function() {
   return gulp.src(PATHS.SOURCE + 'img/*.*')
-    .pipe(gulp.dest(PATHS.DIST + 'img/'));
+    .pipe(gulp.dest(PATHS.BUILD + 'img/'));
 });
 
 gulp.task('css', function () {
-  gulp.src(PATHS.SOURCE + 'css/*.scss')
+  return gulp.src(PATHS.SOURCE + 'css/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest(PATHS.DIST + 'css/'));
+    .pipe(gulp.dest(PATHS.BUILD + 'css/'));
 });
 
 gulp.task('es6to5', function() {
   return gulp.src(PATHS.SOURCE + 'js/**/*.js')
     .pipe(to5())
-    .pipe(gulp.dest(PATHS.DIST + 'js/es5'));
+    .pipe(gulp.dest(PATHS.BUILD + 'js/es5'));
 });
 
 gulp.task('clean-es6', function() {
-  return gulp.src(PATHS.DIST + 'js/es5/')
+  return gulp.src(PATHS.BUILD + 'js/es5/')
     .pipe(clean());
 });
 
 gulp.task('browserify', ['es6to5'], function() {
-  var b = browserify({
-      entries: [PATHS.DIST + 'js/es5/main.js'],
-      debug: DEVELOPMENT
-    });
-  var bndl = b.bundle();
-  var src = bndl.pipe(source('js/main.js'));
-
-  if (!DEVELOPMENT) {
-    return src
-      .pipe(buffer())
-      .pipe(uglify())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest(PATHS.DIST));
-
-  } else {
-    return src.pipe(gulp.dest(PATHS.DIST));
-  }
+  return browserify({
+      entries: [PATHS.BUILD + 'js/es5/main.js'],
+      debug: development
+    })
+    .bundle()
+    .pipe(source('js/main.js'))
+    .pipe(gulp.dest(PATHS.BUILD));
 });
 
-gulp.task('js', function() {
-  runSequence(
+gulp.task('js', function(cb) {
+  return runSequence(
     'es6to5',
     'browserify',
-    'clean-es6');
+    'clean-es6',
+    cb);
 });
 
-gulp.task('startBuild', function() {
-  runSequence(
+gulp.task('usemin', function() {
+  return gulp.src(PATHS.BUILD + 'index.html')
+    .pipe(usemin({
+      css: [minifyCss(), 'concat'],
+      html: [minifyHtml({empty: true})],
+      js: [uglify()]
+    }))
+    .pipe(gulp.dest(PATHS.DIST));
+});
+
+gulp.task('build', function(cb) {
+  development = deploying ? false : true;
+  return runSequence(
     'clean',
     'html',
     'img',
     'css',
-    'js');
+    'js',
+    cb);
 });
 
-gulp.task('build', function(env) {
-  DEVELOPMENT = true;
-  gulp.run('startBuild');
-});
-
-gulp.task('deploy', function(env) {
-  DEVELOPMENT = false;
-  gulp.run('startBuild');
+gulp.task('deploy', function(cb) {
+  deploying = true;
+  return runSequence(
+    'build',
+    'usemin',
+    cb);
 });
